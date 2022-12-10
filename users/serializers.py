d292164a3a01f5e9fb4bd645d61b2474c1188cff
pedrolibas <pedrogv03@hotmail.com>
2022-12-10 12:30:15 -0300

    feat: model user and route user

---
+++ users/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(validators=[UniqueValidator(User.objects.all(), message="username already taken.")])
+    email = serializers.CharField(validators=[UniqueValidator(User.objects.all(), message="email already registered.")])
+    birthdate = serializers.DateField(allow_null=True, default=None)
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    is_employee = serializers.BooleanField(allow_null=True, default=False)
+    is_superuser = serializers.BooleanField(read_only=True)
+
+
+    def create(self, validated_data: dict):
+        
+
+        if validated_data["is_employee"]:
+            user = User.objects.create_superuser(**validated_data)
+        else:
+            user = User.objects.create_user(**validated_data)
+
+        return user
---
+++ users/views.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView, Response, Request, status
+from .serializers import UserSerializer
+
+class RegisterView(APIView):
+    def get(self, request: Request) -> Response:
+        ...
+
+    def post(self, request: Request) -> Response:
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
